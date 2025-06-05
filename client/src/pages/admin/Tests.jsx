8a6e7f31b093fb034cbd9b9facc49fc1aca8c93c
phuoc7701 <101518171+phuoc7701@users.noreply.github.com>
2025-06-05 19:53:52 +0700

    nvp

--- client/src/pages/admin/Tests.jsx
+++ client/src/pages/admin/Tests.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-const testsPerPage = 10;
+const testsPerPage = 5;
 
 const AdminTests = () => {
   const [tests, setTests] = useState([]);
